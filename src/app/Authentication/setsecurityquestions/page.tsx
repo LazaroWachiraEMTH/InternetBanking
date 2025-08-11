ac2919c619d4a6d60cb2ce847e9925425d8d2ab2
lazo <lwachira742@gmail.com>
2025-08-11 16:39:01 +0300

    changes in pages

---
+++ src/app/Authentication/setsecurityquestions/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>set security questions</div>;
+};
+
+export default page;
---
+++ src/app/Beneficiary/addbeneficiary/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>Add beneficiary</div>;
+};
+
+export default page;
